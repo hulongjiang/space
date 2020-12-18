00fc2b06e3fdf96758fb2de106def34070733e69
longjiang.hu <12345678>
2020-12-18 20:37:57 +0800

    api初始化

---
+++ api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"space/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
